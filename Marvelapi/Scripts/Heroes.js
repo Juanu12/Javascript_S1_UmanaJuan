574ea29017da2a0fca204410537f3438f64b83eb
Juanu12 <juanferumana@hotmail.com>
2025-05-08 22:17:30 -0500

    feat: :sparkles: Showing my api heroes

--- Marvelapi/Scripts/Heroes.js
+++ Marvelapi/Scripts/Heroes.js
@@ -1,43 +1,42 @@
-async function Fetch_Data(){
-    axios.get('https://6818a32a5a4b07b9d1d01b5a.mockapi.io/api/v1/Heroes')
-    .then((response)=>{
-      
-      console.log(response.data)
-  
-    })
-  }
 
+document.getElementById("Heroes-container__Send--button").addEventListener("click", function(e) {
+  e.preventDefault();
+  displayHeroes();
+});
+
+async function Fetch_Data(){
+  try {
+    const response = await axios.get('https://6818a32a5a4b07b9d1d01b5a.mockapi.io/api/v1/Heroes');
+    return response.data;
+  } catch (error) {
+    console.error("Error al obtener datos:", error.message);
+  }
+}
   Fetch_Data()
 async function displayHeroes(){
   try{
   const data = await Fetch_Data()
   const herolist = document.getElementById("Showheroes");
-  herolist = "";
+  herolist.innerHTML = "";
+
+  const seleccion = document.getElementById("Heroes-container__Selector--productora").value;
   data.forEach(heroes => {
-    if (document.getElementById("Heroes-container__Selector").value === "Dc")
-    herolist.innerHTML +=`
-    <div id="Showheroes">
-    <p>${[heroes.NombrePersonaje]}</p>
-    <p>${[heroes.Nombreactor]}</p>
-    <p>${[heroes.Edadactor]}</p>
-    <p>${[heroes.Ubicacion]}</p>
-    <p>${[heroes.Poster]}</p>
-    <p>${[heroes.Fecha]}</p>
-    <p>${[heroes.Productora]}</p>
-    <p>${[heroes.Trajes]}</p>
+    productora = [heroes.Productora]
+    if ((heroes.Productora.toLowerCase() === seleccion.toLowerCase()))
     
-    
-    </div> `
+      herolist.innerHTML += `
+      <div class="hero-card">
+        <p><strong>Personaje:</strong> ${heroes.NombrePersonaje}</p>
+        <p><strong>Actor:</strong> ${heroes.Nombreactor}</p>
+        <p><strong>Edad:</strong> ${heroes.Edadactor}</p>
+        <p><strong>Ubicación:</strong> ${heroes.Ubicacion}</p>
+        <img src="${heroes.Poster}">
+        <p><strong>Fecha:</strong> ${heroes.Fecha}</p>
+        <p><strong>Productora:</strong> ${heroes.Productora}</p>
+      </div>
+    `;
   });
-} catch{
-  console.error("Ocurrió un error:", error.message);
+} catch (error) {
+console.error("Ocurrió un error:", error.message);
+};
 }
-}
-
-document.getElementById("Heroes-container__Send").addEventListener("click" , function(pre) {
-pre.preventDefault()
-
- displayHeroes()
-
-})
-
--- Marvelapi/Scripts/script.js
+++ Marvelapi/Scripts/script.js
@@ -1,3 +1,5 @@
+
+
 async function Fetch_Data(){
   axios.get('https://6818a32a5a4b07b9d1d01b5a.mockapi.io/api/v1/Heroes')
   .then((response)=>{
