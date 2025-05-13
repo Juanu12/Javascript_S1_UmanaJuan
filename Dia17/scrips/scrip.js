bed51bdd3e65aa9617df403b911c40c55e688f56
Juanu12 <juanferumana@hotmail.com>
2025-05-12 21:14:48 -0500

    feat: :sparkles: Improving my js

--- Dia17/scrips/scrip.js
+++ Dia17/scrips/scrip.js
@@ -1,3 +1,21 @@
-async function fetchdata(){
-    data  = await axios.get("")
+const petcontent = document.getElementById ("pets-container")
+
+let accessToken = null;
+
+async function fetchData() {
+    const response = await fetch("https://api.petfinder.com/v2/oauth2/token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: new URLSearchParams({
+            grant_type: "client_credentials",
+            client_id: "TU_CLIENT_ID",
+            client_secret: "TU_CLIENT_SECRET"
+        })
+    });
+
+    const data = await response.json();
+    accessToken = data.access_token;
+    console.log("Token guardado en memoria:", accessToken);
 }
